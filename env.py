e2a1569bacf606fd4616f0471a1f1fd90b563f58
Sunjung Kim <51713430+skim-1@users.noreply.github.com>
2022-08-18 21:09:28 -0500

    added cross

--- env.py
+++ env.py
@@ -13,6 +13,8 @@ width = 40
 height = 40
 vel = 40
 
+psize = 96
+
 endX = 360
 endY = 360
 
@@ -20,27 +22,71 @@ run = True
 
 children = []
 
-for i in range(10):
+for i in range(psize):
     children.append({'moves': [], 'fitness': 0})
     for j in range(30):
         children[i]['moves'].append(random.randint(1,4))
 
-children = genes.test(children)
-print(children)
+gen_one = genes.test(children)
 
-# while run:
-#     pygame.time.delay(100)
+for x in range(100):
+    gen_one = genes.cross(gen_one)
+    # print(gen_one[-1]['fitness'])
+    # print(gen_one[-1]['moves'])
+
+# print(gen_one)
+
+# print(gen_one[-1])
 #
-#     for event in pygame.event.get():
-#         if event.type == pygame.QUIT:
-#             run = False
+# gen_one[-1]['fitness'] = 0
 #
+# for j in range(len(gen_one[-1]['moves'])):
+#     if gen_one[-1]['moves'][j] == 1:
+#         y -= 40
+#     if gen_one[-1]['moves'][j] == 2:
+#         x += 40
+#     if gen_one[-1]['moves'][j] == 3:
+#         y += 40
+#     if gen_one[-1]['moves'][j] == 4:
+#         x -= 40
 #
+#     if x < 0 or x > 360 or y <= 0 or y >= 360:
+#         gen_one[-1]['fitness'] -= 50
+#     elif x == 360 and y == 360:
+#         gen_one[-1]['fitness'] += 100
+#     else:
+#         gen_one[-1]['fitness'] += 10
 #
-#     win.fill((0, 0, 0))
-#     pygame.draw.rect(win, (0, 0, 255), (0, 0, width, height))
-#     pygame.draw.rect(win, (255, 0, 0), (x, y, width, height))
-#     pygame.draw.rect(win, (0, 255, 0), (360, 360, width, height))
-#     pygame.display.update()
+#     print(str(x) + " " + str(gen_one[-1]['fitness']))
 #
-# pygame.quit()
+# print(gen_one[-1]['fitness'])
+#
+# x = 0
+# y = 0
+
+
+for i in gen_one[-1]['moves']:
+    pygame.time.delay(100)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    win.fill((0, 0, 0))
+    pygame.draw.rect(win, (0, 0, 255), (0, 0, width, height))
+    pygame.draw.rect(win, (255, 0, 0), (x, y, width, height))
+    pygame.draw.rect(win, (0, 255, 0), (360, 360, width, height))
+    pygame.display.update()
+
+    print("y: " + str(y) + "\n")
+
+    if i == 1:
+        y -= 40
+    elif i == 2:
+        x += 40
+    elif i == 3:
+        y += 40
+    elif i == 4:
+        x -= 40
+
+pygame.quit()
--- genes.py
+++ genes.py
@@ -1,26 +1,68 @@
-def cmp(dict1, dict2):
-    return dict1['fitness'] < dict2['fitness']
+import random
+
 
 def test(ch):
     for i in range(len(ch)):
         x = 0
         y = 0
+
+        reward = 10
         for j in range(len(ch[i]['moves'])):
             if ch[i]['moves'][j] == 1:
-                y += 40
+                y -= 40
             if ch[i]['moves'][j] == 2:
                 x += 40
             if ch[i]['moves'][j] == 3:
-                y -= 40
+                y += 40
             if ch[i]['moves'][j] == 4:
-                x -= 10
+                x -= 40
 
             if x < 0 or x > 360 or y < 0 or y > 360:
-                ch[i]['fitness'] -= 10
+                ch[i]['fitness'] -= 50
+                reward = 0
+            elif x == 360 and y == 360:
+                ch[i]['fitness'] += 100
             else:
-                ch[i]['fitness'] += 10
-    ch.sort()
+                ch[i]['fitness'] += reward
+                reward -= 0
+
+    ch = sorted(ch, key=lambda i: i['fitness'])
     return ch
 
 
+# change the values in for loops if pop size is adjusted
+def cross(ch):
+    c_point = int(random.random() * 30) + 1
 
+    ch_moves = []
+    chMove_final = []
+    ch_final = []
+
+    for i in ch[-48:]:
+        ch_moves.append(i['moves'])
+        chMove_final.append(i['moves'])
+
+    for i in range(24):
+        in_one = random.randint(0, len(ch_moves) - 1)
+        in_two = random.randint(0, len(ch_moves) - 2)
+
+        for j in range(c_point):
+            if random.random() < .1:
+                if random.random() < .5:
+                    ch_moves[in_two][j] = random.randint(1, 4)
+                else:
+                    ch_moves[in_one][j] = random.randint(1, 4)
+            else:
+                t = ch_moves[in_two][j]
+                ch_moves[in_two][j] = ch_moves[in_one][j]
+                ch_moves[in_one][j] = t
+
+        chMove_final.append(ch_moves[in_one])
+        chMove_final.append(ch_moves[in_two])
+        ch_moves.pop(in_one)
+        ch_moves.pop(in_two)
+
+    for i in chMove_final:
+        ch_final.append({'moves': i, 'fitness': 0})
+    ch_final = test(ch_final)
+    return ch_final
